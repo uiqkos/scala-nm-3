7e3a99e81a3044e1d672179b1e2d36c3ba6ddd42
uiqko <uiqkos@gmail.com>
2021-04-28 15:38:05 +0300

    Удалил лишнее

--- src/main/scala/ru/uiqkos/lab3/interpolation/NewtonInterpolation.scala
+++ src/main/scala/ru/uiqkos/lab3/interpolation/NewtonInterpolation.scala
@@ -1,6 +1,6 @@
 package ru.uiqkos.lab3.interpolation
 
-import ru.uiqkos.lab3.interpolation.NewtonInterpolation.{dividedDifference, dividedDifference2}
+import ru.uiqkos.lab3.interpolation.NewtonInterpolation.dividedDifference
 
 import scala.Function.tupled
 import scala.language.postfixOps
@@ -13,31 +13,6 @@ object NewtonInterpolation {
     if (X.length == 1) Y.head
     else (dividedDifference(X.drop(1), Y.drop(1)) - dividedDifference(X.dropRight(1), Y.dropRight(1))) / (X.last - X.head)
   }
-
-  def dividedDifference_(X: Array[Double], Y: Array[Double]): (Array[Double], Array[Double]) = {
-    println(X.length, Y.length)
-    if (Y.length == 2 && X.length == 1) return (Array(), Array(Y(1) - Y(0)))
-    val (newX, newY) =
-      X.zip(Y.sliding(2))
-        .map(tupled((x, y) => {
-          println(y(1), y(0), x, (y(1) - y(0)) / x)
-          (x, (y(1) - y(0)) / x)
-        }))
-        .toArray
-        .unzip
-    (newX.sliding(2).map(a => a.head + a.last).toArray, newY)
-  }
-
-  def dividedDifference2(distances: Array[Double], Y: Array[Double]): Array[Double] = {
-    assert(distances.length == Y.length - 1)
-    if (distances.length == 1) return Array(Y.last - Y.head)
-
-    distances.zip(Y.sliding(2))
-      .map(tupled(
-        (distance, y) => (y(1) - y(0)) / distance
-      ))
-  }
-
 }
 
 class NewtonInterpolation(
