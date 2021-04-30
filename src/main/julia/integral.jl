6535ca7081a0407b8decb0a654c1ae9551e915e0
uiqkos <uiqkos@gmail.com>
2021-04-30 10:58:20 +0300

    Лаба 4 на julia

---
+++ src/main/julia/integral.jl
@@ -0,0 +1,59 @@
+rectangular(Y::Array{Float64}, h::Float64 = 0.001) = h * sum(Y[1:length(Y) - 1])
+trapezoid(Y::Array{Float64}, h::Float64 = 0.001) = (h / 2) * (Y[1] + 2 * sum(Y[2:length(Y) - 1]) + Y[length(Y)])
+simpson(Y::Array{Float64}, h::Float64 = 0.001) = (h / 3) * (Y[1] + Y[end] + 2 * sum(Y[2:2:length(Y) - 2]) + 4 * sum(Y[1:2:length(Y)-1]))
+
+function integral(f::Function, a::Float64, b::Float64, I::Function = trapezoid, ϵ::Float64 = 0.001)
+    """
+        Вычисляет определенный интеграл f на интервале (a, b) с заданной точностью ϵ
+    """
+    n = 10
+    while true
+        h = (b - a) / n
+        Y1 = f.(collect(a:h:b)) # Неоптимально
+        Y2 = f.(collect(a:(h / 2):b)) 
+
+        I1, I2 = I(Y1, h), I(Y2, h / 2)
+
+        if abs(I2 - I1) < ϵ
+            println("Оптимальный шаг $h (n = $n) для достижения точности $ϵ")
+            return I2
+        end
+        n = 2n
+    end
+end
+
+a, b = 1., 3.
+
+f(x) = x ^ -0.5 * log(x)
+
+true_int = 0.877501 # from wolfram alpha
+rect_int = integral(f, a, b, rectangular)
+
+println("Точность: $(true_int - rect_int)")
+
+X1 = collect(a:0.001:b)
+X2 = collect(a:0.0005:b)
+Y1 = f.(X1)
+Y2 = f.(X2)
+
+rectangular_int1, rectangular_int2 = rectangular(Y1, 0.001), rectangular(Y2, 0.0005)
+trapezoid_int1, trapezoid_int2 = trapezoid(Y1, 0.001), trapezoid(Y2, 0.0005)
+simpson_int1, simpson_int2 = simpson(Y1, 0.001), simpson(Y2, 0.0005)
+
+println("Точность формулы прямоугольников с шагом 0.001 = $(rectangular_int1 - true_int)")
+println("Точность формулы прямоугольников с шагом 0.0005 = $(rectangular_int2 - true_int)")
+
+println("Точность формулы трапеции с шагом 0.001 = $(trapezoid_int1 - true_int)")
+println("Точность формулы трапеции с шагом 0.0005 = $(trapezoid_int2 - true_int)")
+
+println("Точность формулы симпсона с шагом 0.001 = $(simpson_int1 - true_int)")
+println("Точность формулы симпсона с шагом 0.0005 = $(simpson_int2 - true_int)")
+
+X = [0.145, 0.147, 0.149, 0.151, 0.153]
+Y = [4.97674, 4.99043, 5.00391, 5.01730, 5.03207]
+
+println("Определенный интеграл от $(X[1]) до $(X[end]) по формуле прямоугольников = ", rectangular(Y, X[2] - X[1]))
+println("Определенный интеграл от $(X[1]) до $(X[end]) по формуле трапеций = ", trapezoid(Y, X[2] - X[1]))
+println("Определенный интеграл от $(X[1]) до $(X[end]) по формуле симпсона = ", simpson(Y, X[2] - X[1]))
+
+# ([Y] .|> [rectangular, trapezoid, simpson]) .|> int -> println(int - true_int)
--- src/main/julia/interpolation.jl
+++ src/main/julia/interpolation.jl
@@ -1,4 +1,8 @@
-using Polynomials, Plots
+using Plots, Polynomials
+without(X::Array{Float64}, i::Int) = if i > 1 cat(dims=1, X[1:(i - 1)], X[(i + 1):end]) else X[2:end] end
+basis(X::Array{Float64}, i::Int) = P = fromroots(without(X, i)) / fromroots(without(X, i))(X[i])
+lagrange(X::Array{Float64}, Y::Array{Float64}) = +((basis.(fill(X, length(X)), 1:length(X)) .* Y)...)
+
 polynomial(X::Array{Float64}, Y::Array{Float64}) = Polynomial(transpose(vcat(fill(X, length(X))...) .^ collect(length(X)-1:-1:0)) / Y)
 function spline(X::Array{Float64}, Y::Array{Float64}, dim::Int = 2)
     intervals = collect(view.(X, (:).(1:length(X) - 1, dim:length(X))))
@@ -12,3 +16,10 @@ Y = [1.2, -2.2, 3.2, -4.2, 5.2, -6.2]
 
 scatter(X, Y)
 plot!(X, spline(X, Y, 2))
+
+X = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0]
+Y = [1.0, -2.0, 3.0, -4.0, 5.0, -6.0]
+
+l = lagrange(X, Y)
+scatter(X, Y)
+plot!(1:0.001:10, l.(1:0.001:10), ylims=(-10, 10))
